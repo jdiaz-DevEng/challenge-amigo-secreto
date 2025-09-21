0ef4806a307b40f73a95f15972c531a24fdb4c86
jdiaz-DevEng <dijean2015@gmail.com>
2025-09-21 20:45:49 -0300

    Definicion de función para agregar un amigo

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 let amigos = [];
 
 let maxAmigos = 10;
+
+function agregarAmigo() {
+    let input = document.getElementById("amigo");
+    let nombre =input.ariaValueMax.trim();
+    let regex = /^[a-zA-Z-áéíóúÁÉÍÓÚñÑ\s]+$/;
+    
+}
